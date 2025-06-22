af309b74d5e47e0f49e2b4f9735b93b7d3b380ed
Ondrej Marek <xmarek9@fi.muni.cz>
2025-06-22 20:48:40 +0200

    feat: add exception system

--- ac-backend/src/core/repository/exception.py
+++ ac-backend/src/core/repository/exception.py
@@ -1,9 +1,10 @@
-from src.core.exception.exceptions import ACException
 from http import HTTPStatus
 
+from src.core.exception.exceptions import ACException
 
-class DatabaseSessionManagerException(ACException):
-    """Base exception for DB session manager errors."""
+
+class RepositoryException(ACException):
+    """Base exception for repository errors."""
 
     CODE = "DB_SESSION_MANAGER_ERROR"
     HTTP_STATUS = HTTPStatus.INTERNAL_SERVER_ERROR
