8b25819bd1a1217078f7777cf4f4fc73f0b06dfd
Ondrej Marek <xmarek9@fi.muni.cz>
2025-08-26 12:19:35 +0200

    feat: add zooming based on selected range

--- ac-frontend/src/components/search/lightcurve/LightCurvePlot.tsx
+++ ac-frontend/src/components/search/lightcurve/LightCurvePlot.tsx
@@ -1,15 +1,19 @@
-import Plot from 'react-plotly.js';
+//import Plot from 'react-plotly.js';
 import * as React from "react";
 import type {PhotometricDataDto} from "@/features/search/lightcurve/types.ts";
 import {colorFromId} from "@/utils/color.ts";
-import {useMemo} from "react";
+import {useContext, useEffect, useMemo} from "react";
 import "@/styles/lightcurve.css";
+import {OptionsContext} from "@/components/search/lightcurve/OptionsContext.tsx";
+import {SetRangeContext} from './CurrentRangeContext';
+import createPlotlyComponent from 'react-plotly.js/factory';
+import Plotly from 'plotly.js-dist-min';
+
+const Plot = createPlotlyComponent(Plotly);
 
 type LightCurvePlotProps = {
-    showErrorBars: boolean,
     pluginNames: Record<string, string>,
     lightCurveData: PhotometricDataDto[]
-    groupBy: string,
 }
 
 type LCData = {
@@ -22,19 +26,22 @@ type LCData = {
 const unknownBand = "Unknown"
 
 
-const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: LightCurvePlotProps) => {
+const LightCurvePlot = ({pluginNames, lightCurveData}: LightCurvePlotProps) => {
+    const optionsContext = useContext(OptionsContext);
+    const rangeContext = useContext(SetRangeContext);
+
     const sourceGroupedLcData = useMemo(() => {
         const groupedLcData: Record<string, LCData> = {};
 
         lightCurveData.forEach((dto) => {
             if (dto.plugin_id in groupedLcData) {
-                groupedLcData[dto.plugin_id].jds.push(dto.julian_date);
+                groupedLcData[dto.plugin_id].jds.push(dto.julian_date - 2400000); // convert to MJD
                 groupedLcData[dto.plugin_id].mags.push(dto.magnitude);
                 groupedLcData[dto.plugin_id].magErrors.push(dto.magnitude_error);
                 groupedLcData[dto.plugin_id].bands.push(dto.light_filter ?? unknownBand)
             } else {
                 groupedLcData[dto.plugin_id] = {
-                    jds: [dto.julian_date],
+                    jds: [dto.julian_date - 2400000],
                     mags: [dto.magnitude],
                     magErrors: [dto.magnitude_error],
                     bands: [dto.light_filter ?? unknownBand]
@@ -50,13 +57,13 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
 
         lightCurveData.forEach((dto) => {
             if ((dto.light_filter ?? unknownBand) in bandGroupedLcData) {
-                bandGroupedLcData[dto.light_filter ?? unknownBand].jds.push(dto.julian_date);
+                bandGroupedLcData[dto.light_filter ?? unknownBand].jds.push(dto.julian_date - 2400000); // convert to MJD
                 bandGroupedLcData[dto.light_filter ?? unknownBand].mags.push(dto.magnitude);
                 bandGroupedLcData[dto.light_filter ?? unknownBand].magErrors.push(dto.magnitude_error);
                 bandGroupedLcData[dto.light_filter ?? unknownBand].bands.push(dto.light_filter ?? unknownBand)
             } else {
                 bandGroupedLcData[dto.light_filter ?? unknownBand] = {
-                    jds: [dto.julian_date],
+                    jds: [dto.julian_date - 2400000],
                     mags: [dto.magnitude],
                     magErrors: [dto.magnitude_error],
                     bands: [dto.light_filter ?? unknownBand]
@@ -69,10 +76,10 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
 
 
     const plotData = useMemo(() => {
-        if (groupBy === "sources") {
+        if (optionsContext?.groupBy === "sources") {
             return Object.entries(sourceGroupedLcData).map(([plugin_id, lcData]) => {
                 const hoverTemplate =
-                    showErrorBars ? ('JD: %{x}<br>mag = %{y:.2f} &plusmn; %{error_y.array:.3f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
+                    optionsContext?.showErrorBars ? ('JD: %{x}<br>mag = %{y:.2f} &plusmn; %{error_y.array:.3f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
                     ) : (
                         'JD: %{x}<br>mag = %{y:.2f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
                     )
@@ -83,7 +90,7 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
                     error_y: {
                         type: "data",
                         array: lcData.magErrors,
-                        visible: showErrorBars,
+                        visible: optionsContext?.showErrorBars,
                         // thickness: 1,
                         // width: 5,
                     },
@@ -99,7 +106,7 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
 
         return Object.entries(bandGroupedLcData).map(([band, lcData]) => {
             const hoverTemplate =
-                showErrorBars ? ('JD: %{x}<br>mag = %{y:.2f} &plusmn; %{error_y.array:.3f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
+                optionsContext?.showErrorBars ? ('JD: %{x}<br>mag = %{y:.2f} &plusmn; %{error_y.array:.3f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
                 ) : (
                     'JD: %{x}<br>mag = %{y:.2f}<br>Band: %{customdata}<br>Source: %{data.name}<extra></extra>'
                 )
@@ -110,7 +117,7 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
                 error_y: {
                     type: "data",
                     array: lcData.magErrors,
-                    visible: showErrorBars,
+                    visible: optionsContext?.showErrorBars,
                     // thickness: 1,
                     // width: 5,
                 },
@@ -122,17 +129,102 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
                 // line: { shape: "linear" },
             }
         });
-    }, [groupBy, showErrorBars, sourceGroupedLcData, bandGroupedLcData])
+    }, [optionsContext?.groupBy, optionsContext?.showErrorBars, sourceGroupedLcData, bandGroupedLcData])
+
+    /*const plotRef = React.useRef<Plotly.PlotlyHTMLElement | null>(null);
+
+    // Apply programmatic zoom from OptionsContext ONCE, imperatively.
+    useEffect(() => {
+        console.log(("fdsfdsfsd"))
+        if (!plotRef.current) return;
+        const hasBounds = optionsContext?.minRange !== undefined && optionsContext?.maxRange !== undefined;
+
+        if (hasBounds) {
+            Plotly.relayout(plotRef.current, {
+                "xaxis.range": [100, 200],
+            });
+        } else {
+            Plotly.relayout(plotRef.current, { "xaxis.autorange": true });
+        }
+    }, [optionsContext?.minRange, optionsContext?.maxRange]);*/
+
+    /*const gdRef = React.useRef<Plotly.PlotlyHTMLElement | null>(null);
+
+    const handleInitialized = React.useCallback((_figure: any, graphDiv: Plotly.PlotlyHTMLElement) => {
+        gdRef.current = graphDiv;
+    }, []);
+
+    const handleUpdate = React.useCallback((_figure: any, graphDiv: Plotly.PlotlyHTMLElement) => {
+        gdRef.current = graphDiv; // keep it fresh after prop-driven updates
+    }, []);
+
+    React.useLayoutEffect(() => {
+        // sanity log
+        console.log('apply programmatic zoom');
+
+        const gd = gdRef.current;
+        if (!gd) return;                       // not initialized yet
+        console.log("bbb")
+
+        if (!gd._fullLayout?.xaxis) return;    // axis not ready yet (rare but safe)
+        console.log("aaaa")
+        const hasBounds =
+            optionsContext?.minRange != null && optionsContext?.maxRange != null;
+
+        const relayout = hasBounds
+            ? { 'xaxis.range': [Number(optionsContext!.minRange), Number(optionsContext!.maxRange)] }
+            : { 'xaxis.autorange': true };
+
+        // In dev/StrictMode, guard against unmount between scheduling and run
+        try {
+            Plotly.relayout(gd, relayout);
+
+        } catch (e) {
+
+            // swallow transient errors during strict re-mounts
+        }
+    }, [optionsContext?.minRange, optionsContext?.maxRange]);*/
+
+    /*useEffect(() => {
+            console.log("Relayout called");
+            console.log(optionsContext?.minRange, optionsContext?.maxRange);
+            Plotly.relayout("lightcurve-plot", { 'xaxis.range': [Number(optionsContext!.minRange), Number(optionsContext!.maxRange)] });
+    }, [optionsContext?.minRange, optionsContext?.maxRange]);*/
+
+    const xaxis = useMemo(() => {
+        return optionsContext?.minRange !== undefined && optionsContext?.maxRange !== undefined ? {
+            title: {text: "Modified Julian Date in TDB"},
+            type: "linear",
+            range: [optionsContext?.minRange, optionsContext?.maxRange]
+        } : {
+            title: {text: "Modified Julian Date in TDB"},
+            type: "linear",
+            autorange: true
+        }
+    }, [optionsContext?.minRange, optionsContext?.maxRange, optionsContext?.plotVersion]);
+
+    const handleRelayout = (eventData: any) => {
+        const newMin = eventData["xaxis.range[0]"];
+        const newMax = eventData["xaxis.range[1]"];
+
+        if (newMin !== undefined && newMax !== undefined) {
+            rangeContext?.setCurrMaxRange(Number(newMax))
+            rangeContext?.setCurrMinRange(Number(newMin))
+        }
+    };
+
+    console.log("rerender")
 
     return (
         <Plot
+            id={"lightcurve-plot"}
+            //ref={plotRef as any}
+            //onInitialized={handleInitialized}
+            //onUpdate={handleUpdate}
             data={plotData}
             layout={{
                 title: {text: "Light Curve"},
-                xaxis: {
-                    title: {text: "Julian Date"},
-                    type: "linear",
-                },
+                xaxis: xaxis,
                 yaxis: {
                     title: {text: "Magnitude"},
                     autorange: "reversed", // Lower mag = brighter
@@ -144,9 +236,10 @@ const LightCurvePlot = ({showErrorBars, pluginNames, lightCurveData, groupBy}: L
                 responsive: true,
                 scrollZoom: true,
                 displaylogo: false,
-                //   modeBarButtonsToRemove: ["lasso2d", "select2d"],
+                modeBarButtonsToRemove: ["lasso2d", "select2d"],
             }}
-            style={{ width: "100%", height: "500px" }}
+            onRelayout={handleRelayout}
+            //style={{ width: "100%", height: "50%" }}
         />
     );
 }
