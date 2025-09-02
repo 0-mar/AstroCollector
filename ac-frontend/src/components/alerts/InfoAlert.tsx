428c7d473caef5c518b57ee7d4d1f7e77c995061
Ondrej Marek <xmarek9@fi.muni.cz>
2025-09-02 18:19:08 +0200

    feat: add InfoAlert

---
+++ ac-frontend/src/components/alerts/InfoAlert.tsx
@@ -0,0 +1,22 @@
+import {Alert, AlertDescription, AlertTitle} from "../../../components/ui/alert.tsx";
+import {Info} from "lucide-react";
+import React from "react";
+
+type InfoAlertProps = {
+    title: string,
+    children: React.ReactNode,
+}
+
+const InfoAlert = ({title, children}: InfoAlertProps) => {
+    return (
+        <Alert className={"text-gray-900 text-left"}>
+            <Info />
+            <AlertTitle>{title}</AlertTitle>
+            <AlertDescription>
+                {children}
+            </AlertDescription>
+        </Alert>
+    )
+}
+
+export default InfoAlert;
