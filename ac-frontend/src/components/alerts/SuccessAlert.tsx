f92a35e4c4200be5a99b93a3714cb31bb9ff8c95
0-mar <77574943+0-mar@users.noreply.github.com>
2025-09-05 21:09:28 +0200

    fix: shadcn css (#51)
    Co-authored-by: Ondrej Marek <xmarek9@fi.muni.cz>

--- ac-frontend/src/components/alerts/SuccessAlert.tsx
+++ ac-frontend/src/components/alerts/SuccessAlert.tsx
@@ -5,10 +5,10 @@ import type {AlertProps} from "@/components/alerts/alertProps.ts";
 
 const SuccessAlert = ({title, description}: AlertProps) => {
     return (
-        <Alert className={"text-green-700 text-left"}>
+        <Alert className={"text-green-700 text-left bg-transparent border-green-900 border-2"}>
             <CircleCheck/>
             <AlertTitle>{title}</AlertTitle>
-            <AlertDescription>
+            <AlertDescription className={"text-green-700"}>
                 {description}
             </AlertDescription>
         </Alert>
--- ac-frontend/src/components/loading/LoadingSkeleton.tsx
+++ ac-frontend/src/components/loading/LoadingSkeleton.tsx
@@ -4,12 +4,9 @@ const LoadingSkeleton = ({text}: {text: string}) => {
     return (
         <div className="flex items-center space-x-4">
             <Skeleton className="h-8 w-8 rounded-full" />
-            <div className="space-y-2">
-                <Skeleton className="h-4 w-[200px]" />
-                <p className="text-sm text-muted-foreground animate-pulse">
-                    {text}
-                </p>
-            </div>
+            <p className="text-sm text-muted-foreground animate-pulse">
+                {text}
+            </p>
         </div>
     )
 }
