6c31dea20293eb527c8895b891a230dcffead35a
Ondrej Marek <xmarek9@fi.muni.cz>
2025-09-09 13:23:21 +0200

    feat: change fov & add simbad and NED layers

--- ac-frontend/src/components/search/form/AladinCutout.tsx
+++ ac-frontend/src/components/search/form/AladinCutout.tsx
@@ -1,17 +1,40 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import {SearchFormContext} from "@/components/search/form/SearchFormContext.tsx";
+import ErrorAlert from "@/components/alerts/ErrorAlert.tsx";
 
 const AladinCutout = () => {
     const searchFormContext = useContext(SearchFormContext)
+    const [showError, setShowError] = useState(false)
 
     useEffect(() => {
-        // TODO what if script loading fails and A is not defined?
-        let aladin = A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:60, target:  searchFormContext?.searchValues.objectName ?? `${searchFormContext?.searchValues.rightAscension} ${searchFormContext?.searchValues.declination}`})
-        //aladin.setFov(1)
+        // TODO: NED as well?
+        let declared;
+        try {
+            A;
+            declared = true;
+        } catch(e) {
+            declared = false;
+        }
+
+        if (declared) {
+            setShowError(false);
+        } else {
+            setShowError(true);
+        }
+
+        const target = searchFormContext?.searchValues.objectName ?? `${searchFormContext?.searchValues.rightAscension} ${searchFormContext?.searchValues.declination}`
+        let aladin = A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov: 2 / 60, target: target})
+        aladin.addCatalog(A.catalogFromSimbad(target, 2 / 60, {onClick: 'showTable'}));
+        aladin.addCatalog(A.catalogFromNED(target, 2 / 60, {onClick: 'showPopup', shape: 'plus'}));
+        aladin.setFov(2/60)
     }, [])
 
+    if (showError) {
+        return <ErrorAlert title={"Failed to load Aladin Lite"} description={"Aladin Lite is not available."} />
+    }
+
     return (
-        <div id='aladin-lite-div' style={{ width: '100%', height: '100%' }} />
+        <div id='aladin-lite-div' style={{ width: '506px', height: '506px' }} />
     )
 }
 
