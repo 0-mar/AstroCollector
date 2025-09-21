a74e24eefa7f4c187d559196694697ce2a702b5d
Ondrej Marek <xmarek9@fi.muni.cz>
2025-09-21 13:35:31 +0200

    feat: redirect if logged in

--- ac-frontend/src/routes/login/index.tsx
+++ ac-frontend/src/routes/login/index.tsx
@@ -1,8 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router"
+import {createFileRoute, redirect} from "@tanstack/react-router"
 import LoginForm from "@/components/auth/LoginForm.tsx";
 
 export const Route = createFileRoute('/login/')({
     component: Login,
+    validateSearch: (search) => ({
+        redirect: (search.redirect as string) || '/',
+    }),
+    beforeLoad: ({ context, search }) => {
+        // Redirect if already authenticated
+        if (context.auth.isAuthenticated) {
+            throw redirect({ to: search.redirect })
+        }
+    },
 })
 
 function Login(){
