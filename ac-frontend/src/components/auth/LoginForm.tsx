55510d1168277aa7fb286895203f671a7e4e7d4e
Ondrej Marek <xmarek9@fi.muni.cz>
2025-09-21 13:34:57 +0200

    refactor: login hook

--- ac-frontend/src/components/auth/LoginForm.tsx
+++ ac-frontend/src/components/auth/LoginForm.tsx
@@ -3,17 +3,11 @@ import {yupResolver} from "@hookform/resolvers/yup";
 import {loginFormSchema, type LoginValues} from "@/features/auth/schemas.ts";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "../../../components/ui/form.tsx";
 import {Input} from "../../../components/ui/input.tsx";
-import {type Tokens} from "@/features/auth/useAuth.ts";
 import {Button} from "../../../components/ui/button.tsx";
-import {useMutation} from "@tanstack/react-query";
-import BaseApi from "@/features/api/baseApi.ts";
-import {toast} from "sonner";
-import {useContext} from "react";
-import {AuthContext, type User} from "@/components/auth/AuthContext.tsx";
-import {setHeaderToken} from "@/features/api/refresh.ts";
+import useLogin from "@/features/auth/useLogin.ts";
 
 const LoginForm = () => {
-    const authContext = useContext(AuthContext)
+    const loginMutation = useLogin()
 
     const form = useForm<LoginValues>({
         resolver: yupResolver(loginFormSchema),
@@ -23,43 +17,11 @@ const LoginForm = () => {
         },
     })
 
-    //const [accessToken, loginMutation] = useAuth()
-
-    const loginMutation = useMutation({
-        mutationFn: ({email, password}: {email: string, password: string}) => {
-            const params = new URLSearchParams();
-            params.append("username", email);
-            params.append("password", password);
-            return BaseApi.post<Tokens>(`/security/login`, params, { headers: { "Content-Type": "application/x-www-form-urlencoded" } })
-        },
-        onError: (_error) => {
-            toast.error("Login failed")
-        },
-        onSuccess: (data) => {
-            authContext?.setAccessToken(data.access_token)
-            setHeaderToken(data.access_token)
-            console.log(data)
-        },
-    });
-
     const onSubmit = async (formData: LoginValues) => {
         await loginMutation.mutateAsync({email: formData.email, password: formData.password})
     }
 
-    const userMutation = useMutation({
-        mutationFn: () => {
-            return BaseApi.get<User>("/security/me")
-        },
-        onError: (_error) => {
-            toast.error("Login failed")
-        },
-        onSuccess: (data) => {
-            console.log(data)
-        },
-    });
-
     return (
-        <>
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
                 <FormField
@@ -91,9 +53,6 @@ const LoginForm = () => {
                 <Button type="submit">Log in</Button>
             </form>
         </Form>
-            <Button onClick={() => userMutation.mutateAsync()
-            }>Get Info</Button>
-        </>
     )
 }
 
---
+++ ac-frontend/src/features/auth/useLogin.ts
@@ -0,0 +1,35 @@
+import {useAuth} from "@/features/auth/useAuth.ts";
+import {useNavigate, useSearch} from "@tanstack/react-router";
+import {useMutation} from "@tanstack/react-query";
+import BaseApi from "@/features/api/baseApi.ts";
+import type {Tokens} from "@/features/auth/types.ts";
+import {toast} from "sonner";
+import {setHeaderToken} from "@/features/api/refresh.ts";
+
+const useLogin = () => {
+    const auth = useAuth()
+    const navigate = useNavigate()
+    const redirect = useSearch({from: "/login/"}) as { redirect?: string };
+
+    const loginMutation = useMutation({
+        mutationFn: ({email, password}: {email: string, password: string}) => {
+            const params = new URLSearchParams();
+            params.append("username", email);
+            params.append("password", password);
+            return BaseApi.post<Tokens>(`/security/login`, params, { headers: { "Content-Type": "application/x-www-form-urlencoded" } })
+        },
+        onError: (_error) => {
+            toast.error("Login failed")
+        },
+        onSuccess: async (data) => {
+            auth?.setAccessToken(data.access_token)
+            setHeaderToken(data.access_token)
+            const redirectPath = redirect?.redirect;
+            await navigate({ to: redirectPath ?? "/", replace: true });
+        },
+    });
+
+    return loginMutation
+}
+
+export default useLogin
