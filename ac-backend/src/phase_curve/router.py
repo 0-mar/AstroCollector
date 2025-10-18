7364f1185382c5bc41e88dffe9714cb91b745a8d
Ondrej Marek <xmarek9@fi.muni.cz>
2025-10-18 14:57:08 +0200

    change: http client

--- ac-backend/src/phase_curve/router.py
+++ ac-backend/src/phase_curve/router.py
@@ -1,8 +1,10 @@
 from astropy.coordinates import SkyCoord
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from httpx import AsyncClient
 
 from src.core.exception.exceptions import APIException
-from src.core.http_client import HttpClient
+
+from src.deps import get_http_client
 from src.phase_curve.schemas import PhaseCurveDataDto
 
 
@@ -52,12 +54,16 @@ def get_phase_curve_data(query_data, search_coords: SkyCoord):
 
 @router.get("")
 async def phase_curve_data(
-    name: str | None = None, ra_deg: float | None = None, dec_deg: float | None = None
+    http_client: AsyncClient = Depends(get_http_client),
+    name: str | None = None,
+    ra_deg: float | None = None,
+    dec_deg: float | None = None,
 ) -> PhaseCurveDataDto:
     """
     Get the period and epoch of a star given by its name, or coordinates.
     If both are provided, the name is used as first and if the search fails, the coordinates are used.
     VSX catalog (https://vsx.aavso.org/) is used to get the data.
+    :param http_client:
     :param name:
     :param ra_deg:
     :param dec_deg:
@@ -67,13 +73,11 @@ async def phase_curve_data(
     if name is None and (ra_deg is None or dec_deg is None):
         raise APIException("Provide name or ra_deg and dec_deg in query params.")
 
-    http_client = HttpClient().get_session()
-
     if name is not None:
-        async with http_client.get(
+        query_resp = await http_client.get(
             f"https://vsx.aavso.org/index.php?view=api.object&ident={name}&format=json"
-        ) as query_resp:
-            query_data = await query_resp.json()
+        )
+        query_data = query_resp.json()
 
         if query_data["VSXObject"] != []:
             record = query_data["VSXObject"]
@@ -91,10 +95,10 @@ async def phase_curve_data(
                 )
 
     if ra_deg is not None and dec_deg is not None:
-        async with http_client.get(
+        query_resp = await http_client.get(
             f"https://vsx.aavso.org/index.php?view=api.list&ra={ra_deg}&dec={dec_deg}&radius={30 / 3600}&format=json"
-        ) as query_resp:
-            query_data = await query_resp.json()
+        )
+        query_data = query_resp.json()
 
         return get_phase_curve_data(query_data, SkyCoord(ra_deg, dec_deg, unit="deg"))
 
